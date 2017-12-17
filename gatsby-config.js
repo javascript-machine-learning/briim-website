9647dd45b3539f291c573a242716a0e87f290654
Robin Wieruch <wrobin@gmx.net>
2017-12-17 09:51:30 +0800

    remove offline plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,44 +19,6 @@ module.exports = {
         anonymize: true,
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "gatsby-movement",
-        short_name: "gatsby-movement",
-        start_url: "/",
-        background_color: "#000000",
-        theme_color: "#ffffff",
-        display: "minimal-ui",
-        icons: [
-          {
-            // Everything in /static will be copied to an equivalent
-            // directory in /public during development and build, so
-            // assuming your favicons are in /static/favicons,
-            // you can reference them here
-            src: `/favicons/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/favicons/android-chrome-256x256.png`,
-            sizes: `256x256`,
-            type: `image/png`,
-          },
-          {
-            src: `/favicons/android-chrome-32x32.png`,
-            sizes: `32x32`,
-            type: `image/png`,
-          },
-          {
-            src: `/favicons/android-chrome-16x16.png`,
-            sizes: `16x16`,
-            type: `image/png`,
-          },
-        ],
-      },
-    },
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-offline`,
   ],
 };
